e6d9a064db2de71e48bec582bdab5bc6328bfedc
Ryan Vogel <89211796+R44VC0RP@users.noreply.github.com>
2024-08-26 14:46:06 +0000

    update the default init

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+//App.js
+
+import axios from 'axios';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+// eslint-disable-next-line
+import Home from './components/Home';
+import Cart from './components/Cart';
+
+//data will be the string we send from our server
+// const apiCall = () => {
+//   axios.get(process.env.REACT_APP_BACKEND_URL).then((data) => {
+//     //this console.log will be in our frontend console
+//     console.log(data)
+//   })
+// }
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline">
+                Hello Create React!
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
