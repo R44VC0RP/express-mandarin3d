dac8a356de62b8264c8d4cf041d8b1a3174a9583
R44VC0RP <n01508660@unf.edu>
2024-08-26 15:39:13 -0400

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='row'>
             <div className="col-4">
-                <img src="/public/images/logo.svg" alt="logo" />
+                <img src="%PUBLIC_URL%/public/images/logo.svg" alt="logo" />
             </div>
         </div>
     );
