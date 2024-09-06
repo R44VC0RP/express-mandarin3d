bbfaec152dfc495f134357d8f0e085e6b2fc2583
Ryan Vogel <89211796+R44VC0RP@users.noreply.github.com>
2024-09-06 20:21:37 +0000

    update for analytics

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AptabaseProvider } from '@aptabase/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AptabaseProvider appKey="A-SH-8406584731">
+      <App />
+    </AptabaseProvider>
+
   </React.StrictMode>
 );
 
