69febae96599aaaddc13da42ff9937a16d0ad2ba
R44VC0RP <n01508660@unf.edu>
2024-09-07 09:15:48 -0400

    update for cathcing error

--- client/src/components/ShoppingCartItem.jsx
+++ client/src/components/ShoppingCartItem.jsx
@@ -5,16 +5,21 @@ import { OrbitControls, useGLTF } from '@react-three/drei';
 import { StlViewer } from "react-stl-viewer";
 
 function hexToRgb(hex) {
-  if (hex.startsWith('#')) {
-    hex = hex.substring(1);
+  try {
+    if (hex.startsWith('#')) {
+      hex = hex.substring(1);
   }
   if (hex.length === 3) {
     hex = hex.split('').map(char => char + char).join('');
   }
   const r = parseInt(hex.substring(0, 2), 16);
   const g = parseInt(hex.substring(2, 4), 16);
-  const b = parseInt(hex.substring(4, 6), 16);
-  return { r, g, b };
+    const b = parseInt(hex.substring(4, 6), 16);
+    return { r, g, b };
+  } catch (error) {
+    console.error('Error converting hex to RGB:', error);
+    return { r: 0, g: 0, b: 0 };
+  }
 }
 
 function calculateLuminance(r, g, b) {
