b7840241fe2101ec13e95f0f9856a47072f025e0
R44VC0RP <n01508660@unf.edu>
2024-10-07 14:09:37 -0400

    manual deploy

--- client/src/components/admin/Orders.jsx
+++ client/src/components/admin/Orders.jsx
@@ -198,7 +198,6 @@ export default function Orders() {
           </div>
         </div>
       </main>
-
       {selectedOrder && (
         <div 
           className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
